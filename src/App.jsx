0613ec7144b089cdc5c96bb4579cd9a0a3b5b26f
Billy Overstreet <overstreetbilly@gmail.com>
2024-05-29 11:05:10 -0600

    Added game board component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import "./App.css";
+import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 
 function App() {
     return (
         <main>
             <div id='game-container'>
+                <h1>Tic Tac Toe</h1>
                 <ol id='players'>
                     <Player initialName='Player 1' symbol='X' />
                     <Player initialName='Player 2' symbol='O' />
                 </ol>
-                GAME BOARD
+                <GameBoard />
             </div>
             LOG
         </main>
